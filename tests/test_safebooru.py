9638b235c6b401349914f8f85a8a1fea0b4334e2
boddz <86293196+boddz@users.noreply.github.com>
2022-12-23 21:06:49 +0000

    Update test_safebooru.py

--- tests/test_safebooru.py
+++ tests/test_safebooru.py
@@ -9,13 +9,16 @@ class TestRequestHandler(TestCase):
         self.sb = safebooru2.Safebooru()
         self.post = safebooru2.Posts(id=1038479)
         self.json = self.sb.json_from(self.post)[0]
-    
+
     def test_json_from(self):
         self.assertTrue(type(self.json), dict)
         self.assertTrue("hash" in self.json)
 
     def test_random_id(self):
         self.assertTrue(type(self.sb.random_id), int)
-    
+
     def test_image_ext(self):
         self.assertEqual(self.sb.image_ext(self.json), "p")
+
+    def test_image_ext_full(self):
+        self.assertEqual(self.sb.image_ext_full(self.json), "png")
